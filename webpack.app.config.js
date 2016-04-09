8e99b8771a07acd8c0fd7a1050dd5c2431f5ae80
Michael Barrett <mike182uk@gmail.com>
2016-04-09 15:59:13 +0100

    refactor webpack configs

--- webpack.app.config.js
+++ webpack.app.config.js
@@ -1,65 +1,12 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.app.common.config');
+const deepAssign = require('deep-assign');
 const path = require('path');
 
-const config = {
-  entry: path.resolve(__dirname, 'src/app/index.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'build', 'app'),
-    libraryTarget: 'commonjs2'
-  },
-  externals: ['electron'],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        include: path.join(__dirname, 'src', 'app'),
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        loaders: [
-          'style-loader',
-          'css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.css$/,
-        include: /node_modules/,
-        loaders: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2)(\?\S*)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?\S*)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?\S*)?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?\S*)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/app/index.html')
-    })
-  ],
-  postcss: [
-    require('autoprefixer'),
-    require('postcss-cssnext')
-  ]
-};
+const config = deepAssign({}, commonConfig , {
+    devtool: 'source-map',
+    output: {
+      path: path.join(__dirname, 'build', 'app')
+    }
+});
 
 module.exports = config;
--- webpack.app.dev.config.js
+++ webpack.app.dev.config.js
@@ -1,66 +1,11 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const commonConfig = require('./webpack.app.common.config');
+const deepAssign = require('deep-assign');
 
-const config = {
-  devtool: 'source-map',
-  entry: path.resolve(__dirname, 'src/app/index.js'),
-  output: {
-    filename: 'bundle.js',
-    publicPath: `http://localhost:${process.env.DEV_SERVER_PORT}/`,
-    libraryTarget: 'commonjs2'
-  },
-  externals: ['electron'],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        include: path.join(__dirname, 'src', 'app'),
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        loaders: [
-          'style-loader',
-          'css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.css$/,
-        include: /node_modules/,
-        loaders: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2)(\?\S*)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?\S*)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?\S*)?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?\S*)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/app/index.html')
-    })
-  ],
-  postcss: [
-    require('autoprefixer'),
-    require('postcss-cssnext')
-  ]
-};
+const config = deepAssign({}, commonConfig , {
+    devtool: 'source-map',
+    output: {
+      publicPath: `http://localhost:${process.env.DEV_SERVER_PORT}/`
+    }
+});
 
 module.exports = config;
