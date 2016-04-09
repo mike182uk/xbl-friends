a58d6f35e963870affaba53adf1c2a50fed019f7
Michael Barrett <mike182uk@gmail.com>
2016-04-09 16:26:28 +0100

    remove unnecessary devtool

--- webpack.app.config.js
+++ webpack.app.config.js
@@ -3,7 +3,6 @@ const deepAssign = require('deep-assign');
 const path = require('path');
 
 const config = deepAssign({}, commonConfig , {
-    devtool: 'source-map',
     output: {
       path: path.join(__dirname, 'build', 'app')
     }
