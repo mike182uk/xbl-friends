82fc3d8cd7323bbd6f07bd5854b23a94edb16427
Michael Barrett <mike182uk@gmail.com>
2016-12-21 15:16:08 +0000

    update webpack configs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,7 @@
-const commonConfig = require('./webpack.config.common')
+const baseConfig = require('./webpack.config.base')
 const deepAssign = require('deep-assign')
 
-module.exports = deepAssign({}, commonConfig, {
+module.exports = deepAssign({}, baseConfig, {
   devtool: 'source-map',
   output: {
     publicPath: `http://localhost:${process.env.DEV_SERVER_PORT}/`
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,8 @@
-const commonConfig = require('./webpack.config.common')
+const baseConfig = require('./webpack.config.base')
 const deepAssign = require('deep-assign')
 const path = require('path')
 
-module.exports = deepAssign({}, commonConfig, {
+module.exports = deepAssign({}, baseConfig, {
   output: {
     path: path.join(__dirname, 'build', 'app')
   }
